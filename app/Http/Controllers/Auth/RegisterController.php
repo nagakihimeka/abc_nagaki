00749b48896d3236388434583e27ac5562b0b2bc
永木媛夏 <himeka.8170@gmail.com>
2023-05-16 14:20:40 +0900

    ファイルを追加しました

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use App\Models\Users\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use DB;
+use Carbon\CarbonPeriod;
+
+
+use App\Models\Users\Subjects;
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    public function registerView()
+    {
+        $subjects = Subjects::all();
+        $years = CarbonPeriod::start('2000-01-01')->untilNow()->years()->toArray();
+        $months = CarbonPeriod::start('2000-01-01')->untilNow()->months()->toArray();
+        $days = CarbonPeriod::start('2000-01-01')->untilNow()->days()->toArray();
+
+        return view('auth.register.register', compact('subjects','years','months','days'));
+    }
+
+    public function registerPost(Request $request)
+    {
+
+
+        //バリデーション
+        $validator = Validator::make($request->all(),[
+            'over_name' => 'required|string|max:10',
+            'under_name' => 'required|string|max:10',
+            'over_name_kana' => 'required|string|max:30|regex:/^[ア-ン゛゜ァ-ォャ-ョー]+$/u',
+            'under_name_kana' => 'required|string|max:30|regex:/^[ア-ン゛゜ァ-ォャ-ョー]+$/u',
+            'mail_address' => 'required|max:100|email:filter,dns|unique:users',
+            'sex' => 'required',
+            'old_year' => 'required',
+            'old_month' => 'required',
+            'old_day' => 'required',
+            'role' => 'required',
+            'password' => 'required|between:8,30',
+            'password-con' => 'required|between:8,30|same:password'
+    ]);
+        if ($validator->fails()) {
+            return redirect()->route('registerView')
+            ->withErrors($validator)// Validatorインスタンスの値を$errorsへ保存
+            ->withInput();// 送信されたフォームの値をInput::old()へ引き継ぐ
+        }
+
+
+        DB::beginTransaction();
+        try{
+            $old_year = $request->old_year;
+            $old_month = $request->old_month;
+            $old_day = $request->old_day;
+            $data = $old_year . '-' . $old_month . '-' . $old_day;
+            $birth_day = date('Y-m-d', strtotime($data));
+            $subjects = $request->subject;
+
+            $user_get = User::create([
+                'over_name' => $request->over_name,
+                'under_name' => $request->under_name,
+                'over_name_kana' => $request->over_name_kana,
+                'under_name_kana' => $request->under_name_kana,
+                'mail_address' => $request->mail_address,
+                'sex' => $request->sex,
+                'birth_day' => $birth_day,
+                'role' => $request->role,
+                'password' => bcrypt($request->password)
+            ]);
+            $user = User::findOrFail($user_get->id);
+            $user->subjects()->attach($subjects);//これがあると新規登録ができない
+            DB::commit();
+            return view('auth.login.login');
+        }catch(\Exception $e){
+            DB::rollback();
+            return redirect()->route('loginView');
+        }
+
+
+    }
+}
