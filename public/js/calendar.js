c87b6cd19f37d3b40e939b43fd4ee6718676e7cd
永木媛夏 <himeka.8170@gmail.com>
2023-07-10 21:49:05 +0900

    モーダルに表示させる日付を取得しました。

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -4,15 +4,10 @@ $(function () {
   $('.btn-danger').on('click', function () {
     // モーダルの中身(class="js-modal")の表示
     $('.delete_modal_content').fadeIn();
-    // // 押されたボタンから投稿内容を取得し変数へ格納
-    // var post = $(this).attr('post');
-    // // 押されたボタンから投稿のidを取得し変数へ格納（どの投稿を編集するか特定するのに必要な為）
-    // var post_id = $(this).attr('post_id');
 
-    // // 取得した投稿内容をモーダルの中身へ渡す
-    // $('.modal_post').text(post);
-    // // 取得した投稿のidをモーダルの中身へ渡す
-    // $('.modal_id').val(post_id);
+    //日付取得
+    var title = $(this).val();
+    $("#delete_day").text(title);
     return false;
   });
 
@@ -23,13 +18,3 @@ $(function () {
     return false;
   });
 });
-
-
-$(function () {
-  $('#delete_date').on('click', function () {
-    var title = $('#delete_date').val()
-    $("#delete_day").text(title);
-
-    // console.log(title)
-  })
-})
