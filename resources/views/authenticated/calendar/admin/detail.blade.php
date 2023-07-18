099ddb05bf9d1d397c32cef98b0f5a7dbd962d5b
永木媛夏 <himeka.8170@gmail.com>
2023-07-18 16:03:27 +0900

    予約詳細ページのレイアウトを整えました。

--- resources/views/authenticated/calendar/admin/detail.blade.php
+++ resources/views/authenticated/calendar/admin/detail.blade.php
@@ -3,33 +3,36 @@
 @section('content')
 <div class="w-75 m-auto">
   <div class="w-100">
-    <div class="reserve_date">{{$reserve->setting_reserve}}</div>
-    <table class="reserve_detail">
-      <tr>
-        <th>ID</th>
-        <th>名前</th>
-        <th>場所</th>
-      </tr>
-       @foreach($users as $user)
-      <tr>
-        <td>{{$user->id}}</td>
-        <td>
-          <span>{{$user->over_name}}</span>
-          <span>{{$user->under_name}}</span>
-        </td>
-        <td>
-          @if($reserve->setting_part == 1)
-            <span>リモート</span>
-          @elseif ($reserve->setting_part == 2)
-            <span>リモート</span>
-          @elseif ($reserve->setting_part == 3)
-            <span>リモート</span>
-          @endif
-        </td>
-      </tr>
-       @endforeach
-    </table>
-
+    <div class="reserve_detail">
+      <div class="reserve_date">{{$reserve->setting_reserve}}</div>
+      <div class="reserve_table">
+        <table>
+          <tr class="reserve_users">
+            <th>ID</th>
+            <th>名前</th>
+            <th>場所</th>
+          </tr>
+          @foreach($users as $user)
+          <tr class="reserve_user">
+            <td>{{$user->id}}</td>
+            <td>
+              <span>{{$user->over_name}}</span>
+              <span>{{$user->under_name}}</span>
+            </td>
+            <td>
+              @if($reserve->setting_part == 1)
+                <span>リモート</span>
+              @elseif ($reserve->setting_part == 2)
+                <span>リモート</span>
+              @elseif ($reserve->setting_part == 3)
+                <span>リモート</span>
+              @endif
+            </td>
+          </tr>
+          @endforeach
+        </table>
+      </div>
+</div>
 
 
 
